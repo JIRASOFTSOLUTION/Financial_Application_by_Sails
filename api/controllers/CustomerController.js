53a552f7cc1eb1354cbdc07bc1a12cb8e0e72c33
Jirayu S <jirasoftsolution@gmail.com>
2017-10-28 04:17:50 +0700

    Add custmer create and display

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -6,6 +6,26 @@
  */
 
 module.exports = {
-	
-};
+  'new': function (req, res) {
+    res.view()
+  },
 
+  create: function (req, res, next) {
+    Customer.create(req.params.all(), function customerCreated (err, customer) {
+      if (err) return next(err)
+
+      res.redirect('/customer/show/' + customer.customer_code)
+    })
+  },
+
+  show: function (req, res, next) {
+    Customer.findOne(req.param('id'), function (err, customer) {
+      if (err) return next(err);
+      if (!customer) return next();
+      res.view({
+        customer: customer
+      });
+    });
+  }
+
+}
