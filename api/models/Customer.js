d0673c6b66500e8563b4f84cfc0d15a6c53a0858
Jirayu S <jirasoftsolution@gmail.com>
2017-10-28 03:47:33 +0700

    Add customer model

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -6,9 +6,25 @@
  */
 
 module.exports = {
-
   attributes: {
-
+    customer_code: {
+      type: 'string',
+      size: 10,
+      primaryKey: true,
+      required: true
+    },
+    customer_name: {
+      type: 'string',
+      size: 100,
+      required: true
+    },
+    email: {
+      type: 'string',
+      size: 50,
+      email: true
+    },
+    state: {
+      type: 'string'
+    }
   }
-};
-
+}
